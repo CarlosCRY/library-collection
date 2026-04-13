14a0b41fa35b759d99eda93fdb509c4718946603
CarlosCRY <trueccr@gmail.com>
2026-04-13 08:40:43 +0200

    1 test

--- src/main/java/org/ies/tierno/readers/CustomerReader.java
+++ src/main/java/org/ies/tierno/readers/CustomerReader.java
@@ -26,6 +26,7 @@ public class CustomerReader implements Reader <Customer> {
         scanner.nextLine();
         log.info("Introduzca código ZIP");
         int zipCode = scanner.nextInt();
+        scanner.nextLine();
 
 
         return new Customer(
--- src/test/java/org/ies/tierno/readers/TestCustRead.java
+++ src/test/java/org/ies/tierno/readers/TestCustRead.java
@@ -2,9 +2,11 @@ package org.ies.tierno.readers;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j;
+import org.ies.tierno.models.Customer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 import org.mockito.junit.jupiter.MockitoExtension;
+
 import static org.mockito.Mockito.when;
 
 import java.util.Scanner;
@@ -13,9 +15,22 @@ import java.util.Scanner;
 @AllArgsConstructor
 public class TestCustRead {
     private final Scanner scanner;
+    private final CustomerReader customerReader;
 
     @Test
     public void TestCorrect () {
-        when(scanner.nextLine())
+        when(scanner.nextLine()).thenReturn("123456789X")
+                .thenReturn("Paco")
+                .thenReturn("Gómez");
+        when(scanner.nextInt()).thenReturn(6661488)
+                .thenReturn(28041);
+
+        Customer testCustomer = customerReader.read();
+
+        Assertions.assertEquals("123456789X", testCustomer.getNif());
+        Assertions.assertEquals("Paco", testCustomer.getName());
+        Assertions.assertEquals("Gómez", testCustomer.getSurname());
+        Assertions.assertEquals(6661488, testCustomer.getCNumber());
+        Assertions.assertEquals(28041, testCustomer.getZipCode());
     }
 }
